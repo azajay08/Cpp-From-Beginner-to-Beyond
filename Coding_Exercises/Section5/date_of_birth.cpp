c0a2aead0e24e533f0e2d91fd9a83b99aef85a5d
aaron.p.j@hotmail.com <aaron.p.j@hotmail.com>
2023-08-31 14:27:48 +0300

    finished section 5

---
+++ Coding_Exercises/Section5/date_of_birth.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+void date_of_birth() {
+	int m {};
+	int d {};
+	int y {};
+	//----DO NOT MODIFY THE CODE ABOVE THIS LINE----
+	//---- WRITE YOUR CODE BELOW THIS LINE----
+
+	cin >> m;
+	cin >> d;
+	cin >> y;
+
+	//---- WRITE YOUR CODE ABOVE THIS LINE----
+	//---- Do NOT MODIFY THE CODE BELOW THIS LINE----
+	cout << m << " " << d << " " << y;
+}
---
+++ Section5/BasicIO/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int num1;
+	int num2;
+	double num3;
+	
+//	cout << "Enter a first integar: ";
+//	cin >> num1;
+//	
+//	cout << "Enter a second integar: ";
+//	cin >> num2;
+
+	cout << "Eneter two integars with a space: ";
+	
+	cin >> num1;
+	cin >> num2;
+	
+	cout << "Enter a second double: ";
+	
+	cin >> num3;
+	
+	cout << "The integars you entered are: " << num1 << " and " << num2 << endl;
+	cout << "The double you entered is: " << num3 << endl;
+	
+	return 0;
+}
