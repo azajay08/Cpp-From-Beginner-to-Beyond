5afaaebf1b9acedb9b39c0d05f922714c9f8d47c
aaron.p.j@hotmail.com <aaron.p.j@hotmail.com>
2023-08-31 14:43:39 +0300

    first section 6 ex done

---
+++ Coding_Exercises/Section6/employee_profile.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+void employee_profile() {
+
+	//----WRITE YOUR CODE BELOW THIS LINE----
+
+	int age {0};
+	double hourly_wage {23.50};
+	char* name;
+
+	cin >> name >> age;
+
+
+	//----WRITE YOUR CODE ABOVE THIS LINE----
+	//----DO NOT MODIFY THE CODE BELOW THIS LINE----
+
+	cout << name << " " << age << " " << hourly_wage;
+}
---
+++ Section6/InitializingVariables/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "Enter the room width: ";
+	int room_width {0}; // initialized
+	cin >> room_width;
+	
+	cout << "Enter the room length: ";
+	int room_length {0}; // initialized
+	cin >> room_length;
+	
+	cout << "The area of the room is " << room_width * room_length
+		<< " Square feet" << endl;
+	
+	return 0;
+}
