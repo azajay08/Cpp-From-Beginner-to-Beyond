d0034420c0397b00e0a49557031b9f30548ccea6
aaron.p.j@hotmail.com <aaron.p.j@hotmail.com>
2023-09-19 22:20:21 +0300

    configured projects for vs code

--- Section4/FirstProgram/main.cpp
+++ Section4/FirstProgram/main.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main()
 {
-	int fav_num;
+	int fav_num {100};
 	cout << "Enter your favourite number between 1 and 100:" << endl;
 	cin >> fav_num;
 	cout << "Amazing!! That's my favourite number too!" << endl;
