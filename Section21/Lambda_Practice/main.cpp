67f193e66c02842776f2ae9e0e72485bcc5475bd
aaron.p.j@hotmail.com <aaron.p.j@hotmail.com>
2023-09-28 12:37:42 +0300

    course finished

--- Section21/Lambda_Practice/main.cpp
+++ Section21/Lambda_Practice/main.cpp
@@ -1,7 +1,58 @@
 #include <iostream>
 #include <vector>
-// #include <algorithm>
-// #include <functional>
+#include <algorithm>
+#include <functional>
+
+class Person {
+	friend std::ostream &operator<<(std::ostream &os, const Person &rhs);
+private:
+	std::string name;
+	int age;
+public:
+	Person(std::string name, int age) : name{name}, age{age} {};
+	Person(const Person &p): name{p.name}, age{p.age} { }
+	~Person() = default;
+		std::string get_name() const { return name; }
+	void set_name(std::string name) {this->name = name; };
+	int get_age() const {return age; }
+	void set_age(int age) {this->age = age; }
+};
+
+std::ostream &operator<<(std::ostream &os, const Person &rhs) {
+	os << "[Person: " <<rhs.name << " : " <<rhs.age << "]";
+	return os;
+}
+
+void test5()
+{
+	std::cout << "\n\n---Test5 --------------------------" << std::endl;
+	
+	Person person1("Larry", 18);
+	Person person2("Moe", 30);
+	Person person3("Curly", 25);
+	
+	std::vector<Person> people {person1,person2,person3};
+	
+	// Sort people by name in ascending order
+	std::sort(people.begin(), people.end(), [] (Person lhs, Person rhs) {
+        return lhs.get_name() < rhs.get_name();
+    });
+	
+	// Displays people sorted by name in ascending order
+	for (Person person : people)
+		std::cout << person << std::endl;
+		
+	std::cout << "\n";
+		
+	// Sort people by age in descending order
+	std::sort(people.begin(), people.end(), [] (Person lhs, Person rhs) {
+        return lhs.get_age() > rhs.get_age();
+    });
+	
+	// Displays people sorted by age in descending order
+	for (Person person : people)
+		std::cout << person << std::endl;
+}
 
 void display(std::vector<int> const vec) {
 	for (int x : vec)
@@ -19,5 +70,9 @@ int main() {
 
 	std::transform(vec1.begin(), vec1.end(), vec1.begin(), [](int x) {return x * 100;});
 	display(vec1);
+
+	std::cout << std::endl;
+
+	test5();
 	return 0;
 }
